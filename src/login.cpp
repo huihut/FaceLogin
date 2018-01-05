d1683fb63dbe8cc6e6fe47eadb290d078756d1c1
huihut <huihut@outlook.com>
2018-01-05 23:29:10 +0800

    Annotated QImage2cvMat()

--- src/login.cpp
+++ src/login.cpp
@@ -210,28 +210,24 @@ void Login::cancelButtonClick()
     ui->registerButton->setVisible(true);
 }
 
-/***********************************
-    Register
-***********************************/
-
-cv::Mat Login::QImage2cvMat(QImage image)
-{
-    cv::Mat mat;
-    qDebug() << image.format();
-    switch(image.format())
-    {
-    case QImage::Format_ARGB32:
-    case QImage::Format_RGB32:
-    case QImage::Format_ARGB32_Premultiplied:
-        mat = cv::Mat(image.height(), image.width(), CV_8UC4, (void*)image.constBits(), image.bytesPerLine());
-        break;
-    case QImage::Format_RGB888:
-        mat = cv::Mat(image.height(), image.width(), CV_8UC3, (void*)image.constBits(), image.bytesPerLine());
-        cv::cvtColor(mat, mat, CV_BGR2RGB);
-        break;
-    case QImage::Format_Indexed8:
-        mat = cv::Mat(image.height(), image.width(), CV_8UC1, (void*)image.constBits(), image.bytesPerLine());
-        break;
-    }
-    return mat;
-}
+//cv::Mat Login::QImage2cvMat(QImage image)
+//{
+//    cv::Mat mat;
+//    qDebug() << image.format();
+//    switch(image.format())
+//    {
+//    case QImage::Format_ARGB32:
+//    case QImage::Format_RGB32:
+//    case QImage::Format_ARGB32_Premultiplied:
+//        mat = cv::Mat(image.height(), image.width(), CV_8UC4, (void*)image.constBits(), image.bytesPerLine());
+//        break;
+//    case QImage::Format_RGB888:
+//        mat = cv::Mat(image.height(), image.width(), CV_8UC3, (void*)image.constBits(), image.bytesPerLine());
+//        cv::cvtColor(mat, mat, CV_BGR2RGB);
+//        break;
+//    case QImage::Format_Indexed8:
+//        mat = cv::Mat(image.height(), image.width(), CV_8UC1, (void*)image.constBits(), image.bytesPerLine());
+//        break;
+//    }
+//    return mat;
+//}
